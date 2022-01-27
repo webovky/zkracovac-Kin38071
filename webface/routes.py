add91769f9059dd5a6105aed108cf7a8331dae93
Jan Kindl <kin38071@spseol.cz>
2022-01-27 14:13:06 +0100

    login

--- webface/routes.py
+++ webface/routes.py
@@ -1,10 +1,9 @@
 from . import app
 from .models import User
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
 import functools
 from pony.orm import db_session
-
-# from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 slova = ("Super", "Perfekt", "Úža", "Flask")
 
@@ -26,20 +25,73 @@ def index():
     temp = []
     for user in User.select():
         temp.append((user.nick, user.passwd))
-        print(user.nick)
-        print(user.passwd)
     return render_template("base.html.j2", temp=temp)
 
 
-@app.route("/info/")
-def info():
-    return render_template("info.html.j2")
+@app.route("/add/", methods=['GET'])
+def add():
+    return render_template("add.html.j2")
+
+
+@app.route("/add/", methods=['POST'])
+@db_session
+def add_post():
+    nick = request.form.get('nick')
+    passwd1 = request.form.get('passwd1')
+    passwd2 = request.form.get('passwd2')
+
+    if not all ([nick,passwd1,passwd2]):
+        flash('Musíš vyplnit všechna políčka', "error")
+    else:
+        user = User.get(nick=nick)
+        if user:
+            flash('Tento uživatel již existuje', "error")
+        elif passwd1 != passwd2:
+            flash("Hesla nejsou stejná", "error")
+        else:
+            user = User(nick=nick, passwd=generate_password_hash(passwd1))
+            flash("Uživatel úspěšně vytvořen!", "success")
+            session['nick'] = nick
+
+    return redirect(url_for('add'))
 
 
 @app.route("/abc/")
 def abc():
     return render_template("abc.html.j2", slova=slova)
 
+@app.route("/login/", methods=['GET'])
+@db_session
+def login():
+    login = request.args.get('login')
+    passwd = request.args.get('passwd')
+    return render_template("login.html.j2")
+
+@app.route("/login/", methods=['POST'])
+@db_session
+def login_post():
+    nick = request.form.get('nick')
+    heslo = request.form.get('heslo')
+    user = User.get(nick=nick)
+    next = request.args.get('next')
+    if user and check_password_hash(user.passwd,heslo):
+        session['nick'] = nick
+        flash("úspěšné přihlášení", 'pass')
+        if next:
+            return redirect(next)
+    else:
+        flash("neplatné přihlašovací údaje", 'err')
+    if next:
+        return redirect(url_for("login", next=next))
+    else:
+        return redirect(url_for('login'))
+
+@app.route("/logout/")
+@db_session
+def logout():
+    session.pop('nick', None)
+    flash("Právě jsi se odhlásil", 'pass')
+    return redirect(url_for('login'))
 
 @app.route("/text/")
 def text():
